f7e76f37a7c5f11ebbd5464c40b577f19f2cc8e5
gaetan <andrianjafygaetan@gmail.com>
2024-06-29 17:11:16 +0300

    init

---
+++ app/index.tsx
@@ -0,0 +1,107 @@
+import { TrueSheet } from "@lodev09/react-native-true-sheet";
+import { useRef } from "react";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+const products = [
+  "Shoe",
+  "tee-shirt",
+  "Jeans",
+  "Jacket",
+  "Hat",
+  "Scarf",
+  "Gloves",
+  "Socks",
+  "Sweater",
+  "Shorts",
+];
+export default function Index() {
+  const sheetRef = useRef<TrueSheet>(null);
+  const scrollRef = useRef<ScrollView>(null);
+
+  const { bottom } = useSafeAreaInsets();
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Button
+        title="Show sheet"
+        onPress={() => {
+          sheetRef.current?.present();
+        }}
+      />
+      <TrueSheet
+        ref={sheetRef}
+        sizes={["40%"]}
+        scrollRef={scrollRef}
+        FooterComponent={
+          <TouchableOpacity
+            style={{
+              height: 40,
+              backgroundColor: "teal",
+              bottom: bottom,
+              alignItems: "center",
+              justifyContent: "center",
+              marginHorizontal: 16,
+            }}
+          >
+            <Text
+              style={{
+                fontWeight: "bold",
+                color: "white",
+                textAlign: "center",
+                textAlignVertical: "center",
+                fontSize: 18,
+              }}
+            >
+              Save
+            </Text>
+          </TouchableOpacity>
+        }
+      >
+        <ScrollView nestedScrollEnabled ref={scrollRef}>
+          {products.map((item) => (
+            <View key={item} style={styles.itemContainer}>
+              <Text style={styles.itemText}>{item}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </TrueSheet>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 20,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  itemContainer: {
+    padding: 15,
+    marginBottom: 10,
+    backgroundColor: "#f9f9f9",
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#ddd",
+  },
+  itemText: {
+    fontSize: 18,
+  },
+});
